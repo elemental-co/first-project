e12870b05bd1b565599fc0456cf6acd98dfa8bc5
Tomy Budiman <tomybudiman@protonmail.com>
2019-09-09 23:17:04 +0700

    Updated home route

--- src/views/App.jsx
+++ src/views/App.jsx
@@ -1,11 +1,17 @@
 import React from "react";
+import Loadable from "react-loadable";
 import {BrowserRouter as Router, Route, Switch, Redirect} from "react-router-dom";
+
+const Home = Loadable({
+  loader: () => import("./Home"),
+  loading: () => <React.Fragment/>
+});
 
 const App = () => {
   return(
     <Router>
       <Switch>
-        <Route exact path="/" render={() => "Home"}/>
+        <Route exact path="/" component={Home}/>
         <Route path="/404" render={() => "404"}/>
         <Route render={() => <Redirect to="/404"/>}/>
       </Switch>
