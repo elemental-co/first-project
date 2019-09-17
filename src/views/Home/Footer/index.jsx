b35f796780e9357c5b0174f40c7665f2ed1ffe50
Tomy Budiman <tomybudiman@protonmail.com>
2019-09-17 16:41:20 +0700

    Footer desktop done

--- src/views/Home/Footer/index.jsx
+++ src/views/Home/Footer/index.jsx
@@ -9,9 +9,25 @@ const Footer = () => {
         <div className="left">
           <p className="footer-title"><Translator id="text.phone"/></p>
           <p className="footer-text">+62 812 0000 0000</p>
+          <img alt="Pattern" src={require("../../../assets/image/homeFooter1.svg")}/>
+          <div className="sitemap">
+            <p><Translator id="home.home"/></p>
+            <p><Translator id="home.navbar1"/></p>
+            <p><Translator id="home.navbar2"/></p>
+          </div>
         </div>
-        <div className="center"></div>
-        <div className="right"></div>
+        <div className="center">
+          <p className="footer-title"><Translator id="text.email"/></p>
+          <p className="footer-text">hello@acuralabs.ai</p>
+          <img alt="Pattern" src={require("../../../assets/image/homeFooter1.svg")}/>
+          <img className="logo" alt="Logo" src={require("../../../assets/image/homeHeader3.svg")}/>
+        </div>
+        <div className="right">
+          <p className="footer-title"><Translator id="text.address"/></p>
+          <p className="footer-text">Revenue Tower, 26th Floor, #132, SCBD, South Jakarta 12190</p>
+          <img alt="Pattern" src={require("../../../assets/image/homeFooter1.svg")}/>
+          <p className="copyright">&copy; {new Date().getFullYear()}. <Translator id="footer.copyright"/></p>
+        </div>
       </div>
     </footer>
   )
