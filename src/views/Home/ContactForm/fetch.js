afc7153c51bc718d26c691d019a2f385be690777
Tomy Budiman <tomybudiman@protonmail.com>
2019-10-08 10:50:13 +0700

    Updated fetch function on contact form component

--- src/views/Home/ContactForm/fetch.js
+++ src/views/Home/ContactForm/fetch.js
@@ -4,7 +4,7 @@ export const postContactForm = obj => {
   return new Promise((resolve, reject) => {
     axios({
       method: "POST",
-      url: "http://35.247.178.73:8000/sendmail",
+      url: "http://35.247.178.73:8000/mail/send",
       data: JSON.stringify(obj),
       headers: {"Content-Type": "application/json"}
     }).then(({data}) => {
