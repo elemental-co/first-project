b116111315342b18cc98b2f8191d89df47432ba4
Tomy Budiman <tomybudiman@protonmail.com>
2019-10-28 00:26:29 +0700

    Done

--- src/views/Home/AboutUs/index.jsx
+++ src/views/Home/AboutUs/index.jsx
@@ -1,20 +1,15 @@
 import React, {useState} from "react";
 import {ResizeSensor} from "css-element-queries";
 
-import {mobile, tablet} from "../../../assets/style/_regular.scss";
 import Translator from "Components/Translator";
-import AlternativeData from "./AlternativeData";
+import AlternativeDataV2 from "./AlternativeDataV2";
+import {mobile, tablet} from "../../../assets/style/_regular.scss";
 
 const AboutUs = () => {
   const [screenResolution, setScreenResolution] = useState(null);
   new ResizeSensor(document.getElementsByTagName("html")[0], () => {
     setScreenResolution(window.innerWidth);
   });
-  const howItWorks = [
-    {header: "home.aboutUs.point1.header", content: "home.aboutUs.point1.content"},
-    {header: "home.aboutUs.point2.header", content: "home.aboutUs.point2.content"},
-    {header: "home.aboutUs.point3.header", content: "home.aboutUs.point3.content"}
-  ];
   return(
     <div className="about-us-container" id="home-about-us">
       <div className="who-are-we">
@@ -65,26 +60,15 @@ const AboutUs = () => {
           </div>
         </div>
       </div>
-      <AlternativeData/>
+      <AlternativeDataV2/>
       <div className="how-it-works">
-        <p className="declaration"><Translator id="home.aboutUs.intro"/></p>
-        <p className="declaration"><Translator id="home.aboutUs.title"/></p>
-        <p className="declaration"><Translator id="home.aboutUs.subtitle"/></p>
-        <div className="pipeline-works">
-          {howItWorks.map((each, i) => (
-            <div className="each-item" key={i}>
-              <div className="numeric-container">
-                <hr/>
-                <span className="numeric">
-                  <p>{i + 1}</p>
-                </span>
-              </div>
-              <div className="text-container">
-                <p className="item-header"><Translator id={each.header}/></p>
-                <p className="item-content"><Translator id={each.content}/></p>
-              </div>
-            </div>
-          ))}
+        <div className="left">
+          <p className="declaration"><Translator id="home.aboutUs.intro"/></p>
+          <p className="declaration"><Translator id="home.aboutUs.title"/></p>
+          <p className="declaration"><Translator id="home.aboutUs.subtitle"/></p>
+        </div>
+        <div className="right">
+          <img alt="Process" src={require("../../../assets/image/homeAbout3.svg")}/>
         </div>
       </div>
     </div>
